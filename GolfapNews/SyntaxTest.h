dda4cb964d735814fcdd7e61825fa0fd542b6765
Mark Holton <holtonma@Macintosh-69.local>
2009-02-22 12:34:17 -0800

    playing around with Obj-C

---
+++ GolfapNews/SyntaxTest.h
@@ -0,0 +1,16 @@
+//
+//  SyntaxTest.h
+//  GolfapNews
+//
+//  Created by Mark Holton on 2/22/09.
+//  Copyright 2009 Albatross-Software.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface SyntaxTest : SenTestCase {
+
+}
+
+@end
---
+++ GolfapNews/SyntaxTest.m
@@ -0,0 +1,19 @@
+//
+//  SyntaxTest.m
+//  GolfapNews
+//
+//  Created by Mark Holton on 2/22/09.
+//  Copyright 2009 Albatross-Software.com. All rights reserved.
+//
+
+#import "SyntaxTest.h"
+
+
+@implementation SyntaxTest
+
+- (void) testTrue
+{
+  STAssertTrue(YES, @"Message will be shown on failure"); 
+}
+
+@end
