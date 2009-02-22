e64f4e2f1daa02f68b5321db95730d7f949be9ae
Mark Holton <holtonma@Macintosh-69.local>
2009-02-22 13:03:41 -0800

    playing around with test cases and AssertEquals, AssertEqualsWithAccuracy

--- GolfapNews/SyntaxTest.m
+++ GolfapNews/SyntaxTest.m
@@ -16,4 +16,28 @@
   STAssertTrue(YES, @"Message will be shown on failure"); 
 }
 
+- (void) testString
+{
+  NSString * myString = @"Bacon";
+  STAssertEqualObjects(myString, @"Bacon", @"Got '%@'", myString);
+  STAssertEqualObjects(myString.className, @"NSCFString", @"String expected");
+}
+
+- (void) testObjectCreation
+{
+  NSMutableString * myString = [NSMutableString new];
+  int count = [myString retainCount];
+  STAssertEquals(count, 1, nil);
+  
+  NSDictionary * myDictionary = [[NSDictionary alloc] init];
+  STAssertEquals((int)[myDictionary count], 0, nil);
+  
+  NSNumber * myNumber = [NSNumber numberWithInt:42];
+  STAssertEqualsWithAccuracy((int)[myNumber intValue], 37, 5, nil);
+  
+  [myString release];
+  [myDictionary release];
+}
+    
+
 @end
